892d3560f171c9993536ab56a0786a548abea9b6
Saptadwipa Choudhury <saptadweepa.30@gmail.com>
2024-09-17 09:10:32 +0200

    Added author entity and related supporting classes + tests

---
+++ src/main/java/com/documentService/document/service/AuthorService.java
@@ -0,0 +1,35 @@
+package com.documentService.document.service;
+
+import com.documentService.document.model.Author;
+import com.documentService.document.repository.AuthorRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class AuthorService {
+
+    private AuthorRepository authorRepository;
+
+    public List<Author> findAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Optional<Author> findAuthorById(Long id) {
+        return authorRepository.findById(id);
+    }
+
+    @Transactional
+    public Author saveAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    @Transactional
+    public void deleteAuthor(Long id) {
+        authorRepository.deleteById(id);
+    }
+}
---
+++ src/test/java/com/documentService/document/service/AuthorServiceTest.java
@@ -0,0 +1,91 @@
+package com.documentService.document.service;
+
+import com.documentService.document.model.Author;
+import com.documentService.document.repository.AuthorRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class AuthorServiceTest {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    private AuthorService authorService;
+
+    @BeforeEach
+    void cleanup(){
+        authorRepository.deleteAll();
+    }
+
+    @Test
+    @Transactional
+    void shouldSaveAndFetchAuthor(){
+
+        //GIVEN
+        Author author = new Author();
+        author.setFirstName("TestFirstName");
+        author.setLastName("TestLastName");
+
+        //WHEN
+        Author authorSaved = authorService.saveAuthor(author);
+        Author authorInDb = authorService.findAuthorById(authorSaved.getId()).get();
+
+        //THEN
+        assertThat(authorSaved.getId())
+                .isNotNull();
+        assertThat(authorSaved)
+                .isEqualTo(authorInDb);
+    }
+
+    @Test
+    @Transactional
+    void shouldBeAbleToFetchAllAuthorsInDb(){
+
+        //GIVEN
+        Set<Author> authors = new HashSet<>();
+        for(int i= 0;i<10;i++){
+            Author author = new Author();
+            author.setFirstName("First" + i);
+            author.setLastName("Last" + i);
+            authors.add(author);
+        }
+        authorRepository.saveAll(authors);
+
+        //WHEN
+        List<Author> authorsInDb = authorService.findAllAuthors();
+
+        //THEN
+        assertThat(new HashSet<>(authorsInDb))
+                .isEqualTo(authors);
+
+    }
+
+    @Test
+    void shouldBeAbleToDeleteAuthor(){
+        //GIVEN
+        Author author = new Author();
+        author.setFirstName("TestFirstName");
+        author.setLastName("TestLastName");
+        Author authorSaved = authorService.saveAuthor(author);
+
+        //WHEN
+        authorService.deleteAuthor(authorSaved.getId());
+
+        //THEN
+        assertThat(authorRepository.findById(authorSaved.getId()))
+                .isEmpty();
+    }
+
+
+}
