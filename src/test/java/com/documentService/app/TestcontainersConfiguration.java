b7989f6320a0da03b2e23533bdf557c89b218c19
Saptadwipa Choudhury <saptadweepa.30@gmail.com>
2024-09-17 09:07:50 +0200

    Added postgres and kafka using docker

--- src/test/java/com/documentService/app/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.documentService.app;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
